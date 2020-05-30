92752a544807a5075a05580684171b9464a33811
Giliard Oliveira <giliard72@hotmail.com>
2020-05-30 16:56:51 -0300

    SWAGGER

--- src/swagger.js
+++ src/swagger.js
@@ -1,8 +1,8 @@
 const swaggerAutogen = require('swagger-autogen')()
 
 const outputFile = './swagger_output.json'
-const endpointsFiles = ['./src/routes/products-routes.js','./src/routes/index-routes.j' ]
+const endpointsFiles = ['./src/routes/products-routes.js']
 
 swaggerAutogen(outputFile, endpointsFiles).then(()=>{
-    require('../bin/server.js')
+    require('./bin/server.js')
 })
